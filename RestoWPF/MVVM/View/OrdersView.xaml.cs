a1de912e9b8f43dd5718870ea7f780e168927ee9
tepecam18 <tepecam18@gmail.com>
2022-07-17 13:57:23 +0300

    v1

--- RestoWPF/MVVM/View/OrdersView.xaml.cs
+++ RestoWPF/MVVM/View/OrdersView.xaml.cs
@@ -24,5 +24,10 @@ namespace RestoWPF.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void OrderNoteCM_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
